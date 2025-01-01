2c5df321fbec9e9e6062caeab9d066c75b17042f
Precious <92827167+Ariesdiamond12@users.noreply.github.com>
2025-01-01 16:28:02 +0200

    Updated the code

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -117,7 +117,7 @@ function Login() {
         style={{ transform: "translateY(-5dvh)" }}
       >
         <div className="flex justify-between font-light text-gray-500 w-[40dvw] ms-10">
-          <p>©{year}, zerohunger</p>
+          <p>©{year}, farmcare</p>
           <p>Terms of use</p>
         </div>
       </div>
